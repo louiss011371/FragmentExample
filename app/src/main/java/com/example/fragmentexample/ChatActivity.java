54025b1088707d12ed0c28112256010a3afb7953
louiss011371 <48555957+louiss011371@users.noreply.github.com>
2019-07-11 00:56:21 +0800

    Revert "change ip for desktop"
    This reverts commit 5ada4408f7e2082e96712c0074e96247859ebfa8.
    
    # Conflicts:
    #	osmadpj/.idea/vcs.xml

--- app/src/main/java/com/example/fragmentexample/ChatActivity.java
+++ app/src/main/java/com/example/fragmentexample/ChatActivity.java
@@ -1,61 +1,38 @@
 package com.example.fragmentexample;
 
 import android.content.Intent;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class ChatActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
-    TextView currentUserName;
-    TextView homepageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_chat);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("");
 
         drawer = findViewById(R.id.drawer_layout);
-
-
         setSupportActionBar(toolbar);
         NavigationView navigationView = findViewById(R.id.nav_view);
-
-        homepageText = findViewById(R.id.homePage);
         navigationView.setNavigationItemSelectedListener(this);
 
-
-        View inflatedView = getLayoutInflater().inflate(R.layout.nav_header, null);
-        currentUserName = inflatedView.findViewById(R.id.user_name);
-
-        Toast.makeText(getApplicationContext(), "CurrentUserName= " + currentUserName.getText(), Toast.LENGTH_LONG).show();
-
-
-        drawer.bringToFront();
-        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
-
-//======================================= default show the home fragment  ========================================================//
+        //======================================= default show the home fragment  ========================================================//
     }
 
 
@@ -68,25 +45,26 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         switch (id) {
 
             case R.id.nav_home:
-                Intent home = new Intent(MainActivity.this, MainActivity.class);
+                Intent home = new Intent(ChatActivity.this, MainActivity.class);
                 startActivity(home);
                 break;
             case R.id.nav_createNewItem:
-                Intent createNew = new Intent(MainActivity.this, CreateNewItemActivity.class);
+                Intent createNew = new Intent(ChatActivity.this, CreateNewItemActivity.class);
                 startActivity(createNew);
                 break;
             case R.id.nav_showAll:
-                Intent showAll = new Intent(MainActivity.this, ShowAllItemActivity.class);
+                Intent showAll = new Intent(ChatActivity.this, ShowAllItemActivity.class);
                 startActivity(showAll);
                 break;
             case R.id.nav_chat:
-                Intent chat = new Intent(MainActivity.this, ChatActivity.class);
+                Intent chat = new Intent(ChatActivity.this, ChatActivity.class);
                 startActivity(chat);
                 break;
             // this is done, now let us go and intialise the home page.
             // after this lets start copying the above.
             // FOLLOW MEEEEE>>>
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
@@ -102,6 +80,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
-
     }
+
 }
